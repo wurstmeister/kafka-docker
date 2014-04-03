96d27ea60311af76eff6099e47ae83dcf5a6bb30
wurstmeister <wurstmeister@users.noreply.github.com>
2014-04-03 21:28:19 +0100

    removed broker name

--- start-broker.sh
+++ start-broker.sh
@@ -6,13 +6,12 @@ then
     echo "Zookeeper is already running"
 else
     echo "Starting Zookeeper"
-    docker run -p 49181:2181  -h zookeeper -name zookeeper -d jplock/zookeeper
+    docker run -p 49181:2181  -h zookeeper --name zookeeper -d jplock/zookeeper
 fi
 
 ID=$1
 PORT=$2
 HOST_IP=$3
-NAME="broker-$ID"
 
-docker run -p $PORT:$PORT -link zookeeper:zk -e BROKER_ID=$ID -e HOST_IP=$HOST_IP -h $NAME -name $NAME -e PORT=$PORT -d wurstmeister/kafka:0.8.1
+docker run -p $PORT:$PORT --link zookeeper:zk -e BROKER_ID=$ID -e HOST_IP=$HOST_IP -e PORT=$PORT -d wurstmeister/kafka:0.8.1
 
