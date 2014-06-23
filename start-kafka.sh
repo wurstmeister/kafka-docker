ef43fdb71cc7d6e88b2df3d84cd3c02dd41fea8c
Ian Babrou <ibobrik@gmail.com>
2014-06-23 20:46:11 +0400

    ability to set KAFKA_HEAP_OPTS

--- start-kafka.sh
+++ start-kafka.sh
@@ -3,4 +3,8 @@ sed -r -i "s/(broker.id)=(.*)/\1=$BROKER_ID/g" $KAFKA_HOME/config/server.propert
 sed -r -i "s/#(advertised.host.name)=(.*)/\1=$HOST_IP/g" $KAFKA_HOME/config/server.properties
 sed -r -i "s/^(port)=(.*)/\1=$PORT/g" $KAFKA_HOME/config/server.properties
 
+if [ "$KAFKA_HEAP_OPTS" != "" ]; then
+    sed -r -i "s/^(export KAFKA_HEAP_OPTS)=\"(.*)\"/\1=\"$KAFKA_HEAP_OPTS\"/g" $KAFKA_HOME/bin/kafka-server-start.sh
+fi
+
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
