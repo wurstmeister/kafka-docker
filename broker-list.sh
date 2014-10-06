4b97ccd09485f81b6e9b39b2db63af04327a979c
Wurstmeister <wurstmeister@users.noreply.github.com>
2014-10-06 11:31:51 +0100

    use port to detect running brokers

--- broker-list.sh
+++ broker-list.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-CONTAINER=`docker ps | grep kafkadocker | head -n 1 | awk  '{print $1}'`
+CONTAINER=`docker ps | grep 9092 | head -n 1 | awk  '{print $1}'`
 docker port $CONTAINER 9092 | sed -r "s/(0.0.0.0):(.*)/$HOST_IP:\2/g"
