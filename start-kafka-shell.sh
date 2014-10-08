8d780c0c4c83d621ec06feaadc017d24f87bd057
Alex Varju <alex@varju.ca>
2014-10-08 14:39:26 -0700

    automatically delete the kafka-shell container when exiting

--- start-kafka-shell.sh
+++ start-kafka-shell.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker run -v /var/run/docker.sock:/var/run/docker.sock --link kafkadocker_zookeeper_1:zk -e HOST_IP=$1 -i -t wurstmeister/kafka:latest /bin/bash
-
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock --link kafkadocker_zookeeper_1:zk -e HOST_IP=$1 -i -t wurstmeister/kafka:latest /bin/bash
