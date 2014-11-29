6e1fcaafe96037de5794880a0f0540818a844332
Wurstmeister <wurstmeister@users.noreply.github.com>
2014-11-29 10:27:30 +0000

    pass zk as argument to start-kafka-shell

--- start-kafka-shell.sh
+++ start-kafka-shell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock --link kafkadocker_zookeeper_1:zk -e HOST_IP=$1 -i -t wurstmeister/kafka:latest /bin/bash
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka:latest /bin/bash
