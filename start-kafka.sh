113eb2dbefa00a7be9863d55d41e264e78cc5ec6
Wurstmeister <wurstmeister@users.noreply.github.com>
2016-06-06 16:44:35 +0100

    Fixed shutdown handling

--- start-kafka.sh
+++ start-kafka.sh
@@ -39,8 +39,24 @@ do
   fi
 done
 
-# Capture kill requests to stop properly
-trap "$KAFKA_HOME/bin/kafka-server-stop.sh; echo 'Kafka stopped.'; exit" SIGHUP SIGINT SIGTERM
+KAFKA_PID=0
 
+# see https://medium.com/@gchudnov/trapping-signals-in-docker-containers-7a57fdda7d86#.bh35ir4u5
+term_handler() {
+  echo 'Stopping Kafka....'
+  if [ $KAFKA_PID -ne 0 ]; then
+    kill -s TERM "$KAFKA_PID"
+    wait "$KAFKA_PID"
+  fi
+  echo 'Kafka stopped.'
+  exit
+}
+
+
+# Capture kill requests to stop properly
+trap "term_handler" SIGHUP SIGINT SIGTERM
 create-topics.sh & 
-JMX_PORT=$KAFKA_JMX_PORT $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+JMX_PORT=$KAFKA_JMX_PORT $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties & 
+KAFKA_PID=$!
+
+wait
