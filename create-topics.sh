163e628b014fba34367ad7f736aad551aacaa96f
Shaun Scaling <shaun@shaunscaling.com>
2018-03-09 12:40:03 +0000

    Add shellcheck for create-topics.sh
    * Add outline test for behaviour (to be expanded with cleanup policy test later)

--- create-topics.sh
+++ create-topics.sh
@@ -11,10 +11,10 @@ fi
 start_timeout_exceeded=false
 count=0
 step=10
-while netstat -lnt | awk '$4 ~ /:'$KAFKA_PORT'$/ {exit 1}'; do
+while netstat -lnt | awk '$4 ~ /:'"$KAFKA_PORT"'$/ {exit 1}'; do
     echo "waiting for kafka to be ready"
     sleep $step;
-    count=$(expr $count + $step)
+    count=$((count + step))
     if [ $count -gt $START_TIMEOUT ]; then
         start_timeout_exceeded=true
         break
@@ -26,14 +26,16 @@ if $start_timeout_exceeded; then
     exit 1
 fi
 
+# Expected format:
+#   name:partitions:replicas:cleanup.policy
 IFS=','; for topicToCreate in $KAFKA_CREATE_TOPICS; do
     echo "creating topics: $topicToCreate"
-    IFS=':' read -a topicConfig <<< "$topicToCreate"
+    IFS=':' read -r -a topicConfig <<< "$topicToCreate"
     config=
     if [ -n "${topicConfig[3]}" ]; then
         config="--config cleanup.policy=${topicConfig[3]}"
     fi
-    JMX_PORT='' $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $KAFKA_ZOOKEEPER_CONNECT --replication-factor ${topicConfig[2]} --partitions ${topicConfig[1]} --topic "${topicConfig[0]}" $config --if-not-exists &
+    JMX_PORT='' "$KAFKA_HOME/bin/kafka-topics.sh" --create --zookeeper "$KAFKA_ZOOKEEPER_CONNECT" --replication-factor "${topicConfig[2]}" --partitions "${topicConfig[1]}" --topic "${topicConfig[0]}" "$config" --if-not-exists &
 done
 
 wait
