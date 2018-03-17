eb25ce7aaa1df8f0241ac6d54f6c93cb749f6a1d
sscaling <sscaling@users.noreply.github.com>
2018-03-17 08:33:48 +0000

    Display an error message when mirror cannot be located to download Kafka (#270)
    * Display an error message when mirror cannot be located to download Kafka
    * add shellcheck check

--- download-kafka.sh
+++ download-kafka.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/sh -e
 
-mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
+mirror=$(curl --stderr /dev/null 'https://www.apache.org/dyn/closer.cgi?as_json=1' | jq -r '.preferred')
+
+if [[ -z "$mirror" ]]; then
+	echo "Unable to determine mirror for downloading Kafka, the service may be down"
+	exit 1
+fi
+
 url="${mirror}kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
 wget -q "${url}" -O "/tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
