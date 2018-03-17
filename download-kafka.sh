c962643c1b1bf82975023388d029abd1d49adb31
Shaun Scaling <shaun@shaunscaling.com>
2018-03-17 07:59:48 +0000

    Fix up shellcheck + to travis for verification

--- download-kafka.sh
+++ download-kafka.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 
-mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
+mirror=$(curl --stderr /dev/null 'https://www.apache.org/dyn/closer.cgi?as_json=1' | jq -r '.preferred')
 
-if [ -z "$mirror" ]; then
+if [[ -z "$mirror" ]]; then
 	echo "Unable to determine mirror for downloading Kafka, the service may be down"
 	exit 1
 fi
