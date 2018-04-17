a818c31b4d4999512201ddf18803aa277b0d5688
mielmat <mielczarekmateusz@gmail.com>
2018-04-17 17:49:40 +0200

    extend download-kafka.sh script

--- download-kafka.sh
+++ download-kafka.sh
@@ -1,11 +1,12 @@
 #!/bin/sh -e
 
-mirror=$(curl --stderr /dev/null 'https://www.apache.org/dyn/closer.cgi?as_json=1' | jq -r '.preferred')
+path="kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
+mirror=$(curl --stderr /dev/null 'https://www.apache.org/dyn/closer.cgi?path=/${path}&as_json=1' | jq -r '.preferred')
 
 if [[ -z "$mirror" ]]; then
 	echo "Unable to determine mirror for downloading Kafka, the service may be down"
 	exit 1
 fi
 
-url="${mirror}kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
+url=${mirror}${path}
 wget -q "${url}" -O "/tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
