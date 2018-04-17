54838891305a34c6364b72a1fecefb66b719e3a1
mielmat <mielczarekmateusz@gmail.com>
2018-04-17 18:04:28 +0200

    change single quotes to double quotes

--- download-kafka.sh
+++ download-kafka.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 path="kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
-mirror=$(curl --stderr /dev/null 'https://www.apache.org/dyn/closer.cgi?path=/${path}&as_json=1' | jq -r '.preferred')
+mirror=$(curl --stderr /dev/null "https://www.apache.org/dyn/closer.cgi?path=/${path}&as_json=1" | jq -r '.preferred')
 
 if [[ -z "$mirror" ]]; then
 	echo "Unable to determine mirror for downloading Kafka, the service may be down"
