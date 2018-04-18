9b54bc44acbba135d271be3a4c3d859f19a92bc7
mielmat <mielczarekmateusz@gmail.com>
2018-04-18 20:48:41 +0200

    changes in url concatenation

--- download-kafka.sh
+++ download-kafka.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -e
-
+KAFKA_VERSION=1.1.0
+SCALA_VERSION=2.12
 path="kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
-mirror=$(curl --stderr /dev/null "https://www.apache.org/dyn/closer.cgi?path=/${path}&as_json=1" | jq -r '.preferred')
+url=$(curl --stderr /dev/null "https://www.apache.org/dyn/closer.cgi?path=/${path}&as_json=1" | jq -r '"\(.preferred)\(.path_info)"')
 
-if [[ -z "$mirror" ]]; then
+if [[ -z "$url" ]]; then
 	echo "Unable to determine mirror for downloading Kafka, the service may be down"
 	exit 1
 fi
 
-url=${mirror}${path}
 wget -q "${url}" -O "/tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
