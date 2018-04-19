07482650801eaa765dec306eacaa3ffc339f304f
mielmat <mielczarekmateusz@gmail.com>
2018-04-19 16:40:43 +0200

    remove testing leftovers

--- download-kafka.sh
+++ download-kafka.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -e
-KAFKA_VERSION=1.1.0
-SCALA_VERSION=2.12
+
 path="kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
 url=$(curl --stderr /dev/null "https://www.apache.org/dyn/closer.cgi?path=/${path}&as_json=1" | jq -r '"\(.preferred)\(.path_info)"')
 
