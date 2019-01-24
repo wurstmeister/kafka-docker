289a6aa9c6dfa5b614d297f944a274b139c0b92d
Long Tom <ngvlongit1@gmail.com>
2019-01-24 15:02:43 +0700

    remove temp

--- start-kafka.sh
+++ start-kafka.sh
@@ -98,9 +98,7 @@ fi
 
 # Jolokia 
 if [[ -n "$KAFKA_JOLOKIA_PORT" ]]; then
-    echo $KAFKA_OPTS
     export KAFKA_OPTS="-javaagent:$KAFKA_HOME/libs/jolokia-jvm-1.6.0-agent.jar=port=$KAFKA_JOLOKIA_PORT,host=0.0.0.0 $KAFKA_OPTS"
-    echo $KAFKA_OPTS
 fi
 
 #Issue newline to config file in case there is not one already
