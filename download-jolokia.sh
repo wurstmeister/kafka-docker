e3c14bcbf2ba3668179cd5a387b6d96d7daeb4c2
Long Tom <ngvlongit1@gmail.com>
2019-01-24 11:02:44 +0700

    Fix jolokia path

--- download-jolokia.sh
+++ download-jolokia.sh
@@ -2,4 +2,4 @@
 
 wget "http://search.maven.org/remotecontent?filepath=org/jolokia/jolokia-jvm/1.6.0/jolokia-jvm-1.6.0-agent.jar" -O "/tmp/jolokia-jvm-1.6.0-agent.jar"
 mv "/tmp/jolokia-jvm-1.6.0-agent.jar" /opt/kafka/libs
-chmod a+x /opt/kafka/libs/tmp/jolokia-jvm-1.6.0-agent.jar
+chmod a+x /opt/kafka/libs/jolokia-jvm-1.6.0-agent.jar
