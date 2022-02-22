369a1d2cd5cf08baa4ac600fd3b96b3a45e6be1d
James Olsen <james@inaseq.com>
2022-02-23 09:52:53 +1300

    Fix if statement.

--- download-kafka.sh
+++ download-kafka.sh
@@ -9,7 +9,7 @@ url=$(curl --stderr /dev/null "https://www.apache.org/dyn/closer.cgi?path=/kafka
 
 # Test to see if the suggested mirror has this version, currently pre 2.1.1 versions
 # do not appear to be actively mirrored. This may also be useful if closer.cgi is down.
-if [[ ! $(curl -f -s -r 0-1 "${url}") ]]; then
+if [ ! $(curl -f -s -r 0-1 "${url}") ]; then
     echo "Mirror does not have desired version, downloading direct from Apache"
     url="https://archive.apache.org/dist/kafka/${KAFKA_VERSION}/${FILENAME}"
 fi
