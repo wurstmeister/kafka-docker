8d80440462ab417d5f6c81e08bf007a7c3ac6ddc
Frank Vissing <frank.vissing@se.com>
2022-05-20 14:16:24 +0200

    fix selection of kafka args

--- test/scenarios/runJmxScenario.sh
+++ test/scenarios/runJmxScenario.sh
@@ -4,7 +4,7 @@ set -e -o pipefail
 
 pushd jmx
 # since 3.0.0 a few options has been disabled, start kafka with the propper options
-if [[ "$MAJOR_VERSION" -le "3" ]]; then
+if [[ "$MAJOR_VERSION" -lt "3" ]]; then
     docker-compose -f docker-compose.yml -f docker-compose-kafka.yml up -d zookeeper kafka jmxexporter
 else
     docker-compose -f docker-compose.yml -f docker-compose-kafka3.yml up -d zookeeper kafka jmxexporter
