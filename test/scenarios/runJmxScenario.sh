724866b5ef6d8c2b3925b65239c6512551121306
Frank Vissing <frank.vissing@se.com>
2022-05-20 13:19:34 +0200

    make MAJOR_VERSION accessible for test scripts

--- test/scenarios/runJmxScenario.sh
+++ test/scenarios/runJmxScenario.sh
@@ -3,13 +3,11 @@
 set -e -o pipefail
 
 pushd jmx
-# shellcheck disable=SC1091
-source "/usr/bin/versions.sh"
 # since 3.0.0 a few options has been disabled, start kafka with the propper options
-if [[ "$MAJOR_VERSION" -ge "3" ]]; then
-    docker-compose -f docker-compose.yml -f docker-compose-kafka3.yml up -d zookeeper kafka jmxexporter
-else
+if [[ "$MAJOR_VERSION" -le "3" ]]; then
     docker-compose -f docker-compose.yml -f docker-compose-kafka.yml up -d zookeeper kafka jmxexporter
+else
+    docker-compose -f docker-compose.yml -f docker-compose-kafka3.yml up -d zookeeper kafka jmxexporter
 fi
 
 docker-compose run --rm test
