722fc5c75f9db53aebf9d0b06e837ce838be4461
Frank Vissing <frank.vissing@se.com>
2022-06-30 13:25:44 +0200

    make create topics work both for KAFKA_LISTNERS and BROKER_LIST

--- create-topics.sh
+++ create-topics.sh
@@ -14,22 +14,31 @@ step=10
 
 # shellcheck disable=SC1091
 source "/usr/bin/versions.sh"
+# since 3.0.0 there is no --zookeeper option anymore, so we have to use the
+# --bootstrap-server option.
 if [[ "$MAJOR_VERSION" -ge "3" ]]; then
-    PORT=$(echo "$KAFKA_LISTENERS" | awk -F: '{print $3}' )
+    if [[ -v KAFKA_LISTENERS ]]; then
+        PORT=$(echo "$KAFKA_LISTENERS" | awk -F: '{print $3}' )
+        CONNECT_OPTS="--bootstrap-server localhost:${PORT}"
+    else
+        CONNECT_OPTS="--bootstrap-server ${BROKER_LIST}"
+    fi
 else
     PORT="$KAFKA_PORT"
+    CONNECT_OPTS="--zookeeper ${KAFKA_ZOOKEEPER_CONNECT}"
 fi
 
-
-while netstat -lnt | awk '$4 ~ /:'"$PORT"'$/ {exit 1}'; do
-    echo "waiting for kafka to be ready"
-    sleep $step;
-    count=$((count + step))
-    if [ $count -gt $START_TIMEOUT ]; then
-        start_timeout_exceeded=true
-        break
-    fi
-done
+if [[ ! -v PORT ]]; then
+    while netstat -lnt | awk '$4 ~ /:'"$PORT"'$/ {exit 1}'; do
+        echo "waiting for kafka to be ready"
+        sleep $step;
+        count=$((count + step))
+        if [ $count -gt $START_TIMEOUT ]; then
+            start_timeout_exceeded=true
+            break
+        fi
+    done
+fi
 
 if $start_timeout_exceeded; then
     echo "Not able to auto-create topic (waited for $START_TIMEOUT sec)"
@@ -39,14 +48,6 @@ fi
 # introduced in 0.10. In earlier versions, this will fail because the topic already exists.
 if [[ "$MAJOR_VERSION" == "0" && "$MINOR_VERSION" -gt "9" ]] || [[ "$MAJOR_VERSION" -gt "0" ]]; then
     KAFKA_0_10_OPTS="--if-not-exists"
-fi
-
-# since 3.0.0 there is no --zookeeper option anymore, so we have to use the
-# --bootstrap-server option.
-if [[ "$MAJOR_VERSION" -ge "3" ]]; then
-    CONNECT_OPTS="--bootstrap-server ${PORT}"
-else
-    CONNECT_OPTS="--zookeeper ${KAFKA_ZOOKEEPER_CONNECT}"
 fi
 
 # Expected format:
